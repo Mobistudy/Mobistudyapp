c569143d8958077fe0e8d49368914474b56902f7
jamtholee <jameson.lee@gmx.com>
2018-11-04 13:22:50 +0000

    UNTESTED - updated nativeStorage for server queue functionality

--- src/modules/db_nativeStorage.js
+++ src/modules/db_nativeStorage.js
@@ -1,5 +1,9 @@
 /* eslint-disable standard/no-callback-literal */
 
+// ************************************
+// * THIS ENTIRE CODEBASE IS UNTESTED *
+// ************************************
+
 let storage = window.NativeStorage
 
 export function getUserSession () {
@@ -20,6 +24,39 @@ export function setUserSession (session) {
 
 export function rmUserSession () {
   storage.remove('session', function (res) {
+    return Promise.resolve(true)
+  }, function (err) {
+    return Promise.reject(err)
+  })
+}
+
+export function pushToServerQueue (obj) {
+  storage.getItem('serverQueue', function (res) {
+    res.push(obj)
+    storage.setItem('serverQueue', res, function (res) {
+      return Promise.resolve(true)
+    }, function (err) {
+      return Promise.reject(err)
+    })
+  }, function (e) {
+    storage.setItem('serverQueue', [obj], function (res) {
+      return Promise.resolve(true)
+    }, function (err) {
+      return Promise.reject(err)
+    })
+  })
+}
+
+export function retrieveServerQueue () {
+  storage.getItem('serverQueue', function (res) {
+    return Promise.resolve(res)
+  }, function (e) {
+    return Promise.resolve([])
+  })
+}
+
+export function clearServerQueue () {
+  storage.remove('serverQueue', function (res) {
     return Promise.resolve(true)
   }, function (err) {
     return Promise.reject(err)
