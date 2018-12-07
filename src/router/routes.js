6f28bd0835419d8b6a9b3c355a009c0e9324a320
Dario Salvi <dario.salvi.work@gmail.com>
2018-12-07 16:56:23 +0000

    removed Index.vue

--- src/router/routes.js
+++ src/router/routes.js
@@ -24,8 +24,7 @@ const routes = [
     path: '/home',
     component: () => import('layouts/HomeLayout.vue'),
     children: [
-      { path: '/', component: () => import('pages/Index.vue') },
-      { path: '/index', component: () => import('pages/Index.vue') },
+      { path: '/', component: () => import('pages/Tasker.vue') },
       { path: '/tasker', component: () => import('pages/Main/Tasker.vue') },
       { path: '/about', component: () => import('pages/About.vue') },
       { path: '/profile', component: () => import('pages/Main/Profile.vue') },
