d95de84eb154c1ddca758cb75142d2eb957505e6
Dario Salvi <dario.salvi.work@gmail.com>
2019-02-13 18:01:14 +0000

    fixes #5 fixes #7

--- src/pages/AccountMgmt/Register.vue
+++ src/pages/AccountMgmt/Register.vue
@@ -5,17 +5,42 @@
         <p class="q-headline">Registration</p>
         <q-stepper vertical ref="stepper">
           <q-step title="Terms of Use">
-            <h5>Terms and conditions</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt dictum faucibus. Nunc vel ultricies
-              quam, vel molestie tellus. Curabitur viverra tristique quam, vitae ornare ex pulvinar sed. Vestibulum sagittis
-              eros non ante dignissim, et faucibus turpis auctor. Duis volutpat massa lorem, in luctus lacus rutrum quis.
-              Integer eu urna id quam dignissim ultrices ut vel metus. Proin vel porta velit. Pellentesque dictum tortor
-              mollis pretium aliquet. Curabitur scelerisque augue non posuere tincidunt. Duis vitae lobortis urna. Aliquam
-              mollis turpis ut erat tristique fermentum. Integer sollicitudin posuere mi sed laoreet. In semper quam vitae
-              enim condimentum tincidunt. Proin ac metus maximus, venenatis urna at, rhoncus magna. Mauris leo nunc,
-              faucibus suscipit suscipit id, maximus quis ipsum.
+            <p>
+              This is an alpha version of Mobistudy and is meant to be used for technical
+              testing only. Please do not use this app unless you are a member of the
+              development team of Mobistudy.
             </p>
             <q-stepper-navigation>
+              <q-btn flat @click="$router.push('login')"  label="Back"/>
+              <q-btn color="primary" @click="$refs.stepper.next()" label="Accept" />
+            </q-stepper-navigation>
+          </q-step>
+
+          <q-step icon="lock" title="Privacy policy">
+            <p>
+              Mobistudy will collect the following information from you, regardless of if
+              have joined or not any research study:
+              <ul>
+                <li>Your email address</li>
+                <li>Your name and surname</li>
+                <li>Your date of bith</li>
+                <li>Your gender</li>
+                <li>Your list of self-declared long-term health conditions</li>
+                <li>Your list of self-declared frequently-used medications</li>
+                <li>Your list of self-declared lifestyle flags (e.g. if you smoke)</li>
+              </ul>
+              These data will stored on a server hosted and managed by the University of Oxford.
+              Personnell from the University of Oxford may access your data for technical maintenance.
+              <br />
+              If you decide to join a research study, further study-specific conditions
+              will apply, which will be clearly shown to you when consenting to participate
+              in the study.
+              <br />
+              Under no circumstances rather than the ones specified above, your data will
+              be shared with other parties or individuals.
+            </p>
+            <q-stepper-navigation>
+              <q-btn flat @click="$refs.stepper.previous()"  label="Back"/>
               <q-btn color="primary" @click="$refs.stepper.next()" label="Accept" />
             </q-stepper-navigation>
           </q-step>
@@ -24,66 +49,87 @@
             <q-field icon="mail_outline" :error="$v.account.email.$error" error-label="Please type a valid email">
               <q-input float-label="Email" @blur="$v.account.email.$touch" v-model="account.email"/>
             </q-field>
-            <q-field icon="vpn_key" :error="$v.account.pw1.$error || $v.account.pw2.$error"
-            error-label="Passwords do not match">
-            <q-input float-label="Password" v-model="account.pw1" type="password"/>
-            <q-input float-label="Confirm Password" v-model="account.pw2" @blur="$v.account.pw2.$touch" type="password"/>
-          </q-field>
-          <q-stepper-navigation>
-            <q-btn color="primary" @click="register()" label="Next" />
-          </q-stepper-navigation>
-        </q-step>
+            <q-field icon="vpn_key" :error="$v.account.pw1.$error" :error-label="getFirstPwdCheckError(account.pw1)">
+              <q-input float-label="Password" v-model="account.pw1" @blur="$v.account.pw1.$touch" type="password"/>
+            </q-field>
+            <q-field icon="vpn_key" :error="$v.account.pw2.$error" error-label="Passwords must match">
+              <q-input float-label="Confirm Password" v-model="account.pw2" @blur="$v.account.pw2.$touch" type="password"/>
+            </q-field>
+            <q-stepper-navigation>
+              <q-btn flat @click="$refs.stepper.previous()"  label="Back"/>
+              <q-btn color="primary" :disable="$v.account.$error" @click="register()" label="Next" />
+            </q-stepper-navigation>
+          </q-step>
 
-        <!-- Step: -->
-        <q-step title="Profile" subtitle="Tell us your details">
-          <q-field icon="face" :error="$v.profile.name.$error || $v.profile.surname.$error" error-label="Required">
-            <q-input float-label="First Name" v-model="profile.name"/>
-            <q-input float-label="Surname" v-model="profile.surname"/>
-          </q-field>
-          <q-field icon="wc" :error="$v.profile.gender.$error" error-label="Required">
-            <q-select float-label="Gender" v-model="profile.gender" :options="profile.genderOptions"/>
-          </q-field>
-          <q-field icon="cake" :error="$v.profile.dateOfBirth.$error" error-label="Required">
-            <q-datetime type="date" v-model="profile.dateOfBirth" format="DD/MM/YYYY" float-label="Date of Birth"/>
-          </q-field>
+          <!-- Step: -->
+          <q-step icon="assignment_ind" title="Profile" subtitle="Tell us your details">
+            <q-field icon="face" :error="$v.profile.name.$error || $v.profile.surname.$error" error-label="Required">
+              <q-input float-label="First Name" v-model="profile.name"/>
+              <q-input float-label="Surname" v-model="profile.surname"/>
+            </q-field>
+            <q-field icon="wc" :error="$v.profile.gender.$error" error-label="Required">
+              <q-select float-label="Gender" v-model="profile.gender" :options="profile.genderOptions"/>
+            </q-field>
+            <q-field icon="cake" :error="$v.profile.dateOfBirth.$error" error-label="Required">
+              <q-datetime type="date" v-model="profile.dateOfBirth" format="DD/MM/YYYY" float-label="Date of Birth"/>
+            </q-field>
 
-          <q-field class="q-mt-sm" icon="local_hospital" helper="Do you suffer from any long-term medical condition?">
-            <q-chips-input placeholder="Conditions" v-model="diseasesVue" @duplicate="duplicatedDisease">
-              <q-autocomplete @search="searchDisease" @selected="selectedDisease" />
-            </q-chips-input>
-          </q-field>
+            <q-field class="q-mt-sm" icon="local_hospital" helper="Do you suffer from any long-term medical condition?">
+              <q-chips-input placeholder="Conditions" v-model="diseasesVue" @duplicate="duplicatedDisease">
+                <q-autocomplete @search="searchDisease" @selected="selectedDisease" />
+              </q-chips-input>
+            </q-field>
 
-          <q-field class="q-mt-lg" icon="local_pharmacy" helper="Are you on any long-term medication?">
-            <q-chips-input placeholder="Medications" v-model="medsVue" @duplicate="duplicatedMeds">
-              <q-autocomplete @search="searchMeds" @selected="selectedMeds" />
-            </q-chips-input>
-          </q-field>
+            <q-field class="q-mt-lg" icon="local_pharmacy" helper="Are you on any long-term medication?">
+              <q-chips-input placeholder="Medications" v-model="medsVue" @duplicate="duplicatedMeds">
+                <q-autocomplete @search="searchMeds" @selected="selectedMeds" />
+              </q-chips-input>
+            </q-field>
 
-          <q-toggle class="q-mt-lg q-ma-sm" label="Do you smoke?" v-model="profile.lifestyle.smoker" checked-icon="smoking_rooms" unchecked-icon="smoke_free"/>
-          <q-toggle class="q-ma-sm" label="Do you have an active lifestyle?" v-model="profile.lifestyle.active" checked-icon="directions_run" unchecked-icon="airline_seat_recline_normal"/>
-          <q-stepper-navigation>
-            <q-btn flat @click="$refs.stepper.previous()"  label="Back"/>
-            <q-btn color="primary" @click="saveProfile()"  label="Next" />
-          </q-stepper-navigation>
-        </q-step>
-        <!--
-        Optional.
-
-        "Globally" available Stepper navigation which means
-        that it will be visible regardless of the current step.
-        If we'd put QStepperNavigation under a QStep then we'd
-        be using it for that step only.
-      -->
-    </q-stepper>
-  </q-page>
-</q-page-container>
+            <q-toggle class="q-mt-lg q-ma-sm" label="Do you smoke?" v-model="profile.lifestyle.smoker" checked-icon="smoking_rooms" unchecked-icon="smoke_free"/>
+            <q-toggle class="q-ma-sm" label="Do you have an active lifestyle?" v-model="profile.lifestyle.active" checked-icon="directions_run" unchecked-icon="airline_seat_recline_normal"/>
+            <q-stepper-navigation>
+              <q-btn flat @click="$refs.stepper.previous()"  label="Back"/>
+              <q-btn color="primary" @click="saveProfile()"  label="Next" />
+            </q-stepper-navigation>
+          </q-step>
+      </q-stepper>
+    </q-page>
+  </q-page-container>
 </q-layout>
 </template>
 
 <script>
+import PWDchecker from 'zxcvbn'
+import owasp from 'owasp-password-strength-test'
 import API from '../../modules/API'
 import userinfo from '../../modules/userinfo'
 import {required, email, sameAs} from 'vuelidate/lib/validators'
+
+owasp.config({
+  allowPassphrases: true,
+  maxLength: 70,
+  minLength: 8,
+  minPhraseLength: 10,
+  minOptionalTestsToPass: 3
+})
+
+function checkPwdStrength (pwd) {
+  if (this.account.email) {
+    // check if password includes name in email
+    let i = this.account.email.indexOf('@')
+    if (i > 0) {
+      let userName = this.account.email.substring(0, i)
+      if (pwd.toUpperCase().includes(userName.toUpperCase())) {
+        return false
+      }
+    }
+  }
+  if (owasp.test(pwd).strong) {
+    let strength = PWDchecker(pwd)
+    return strength.score >= 2
+  } else return false
+}
 
 export default {
   name: 'RegisterPage',
@@ -100,7 +146,10 @@ export default {
         dateOfBirth: '',
         diseases: [],
         medications: [],
-        lifestyle: {},
+        lifestyle: {
+          smoker: false,
+          active: true
+        },
         gender: '',
         genderOptions: [
           {
@@ -122,10 +171,8 @@ export default {
   validations: {
     account: {
       email: {required, email},
-      pw1: {required},
-      pw2: {
-        sameAsPassword: sameAs('pw1')
-      }
+      pw1: { required, checkPwdStrength },
+      pw2: { sameAsPassword: sameAs('pw1') }
     },
     profile: {
       name: {required},
@@ -157,6 +204,32 @@ export default {
     }
   },
   methods: {
+    getFirstPwdCheckError (pwd) {
+      if (this.account.email) {
+        // check if password includes name in email
+        let i = this.account.email.indexOf('@')
+        if (i > 0) {
+          let userName = this.account.email.substring(0, i)
+          if (pwd.toUpperCase().includes(userName.toUpperCase())) {
+            return 'Password cannot contain email'
+          }
+        }
+      }
+      let result = owasp.test(pwd)
+      if (!result.strong) {
+        return result.errors[0]
+      } else {
+        result = PWDchecker(pwd)
+        if (result.feedback) {
+          let mesg = 'The password is too simple'
+          if (result.feedback.warning) mesg = result.feedback.warning
+          // if (result.feedback.suggestions && result.feedback.suggestions.length) {
+          //   mesg += '.\nSuggestion: ' + result.feedback.suggestions[0]
+          // }
+          return mesg
+        } else return 'All OK'
+      }
+    },
     async searchDisease (diseaseDescription, done) {
       try {
         const results = await API.searchSNOMEDDisease(diseaseDescription)
@@ -211,7 +284,7 @@ export default {
     },
     async register () {
       this.$v.account.$touch()
-      if (this.$v.account.error) {
+      if (this.$v.account.$error) {
         this.$q.notify('Please correct the indicated fields.')
       } else {
         try {
@@ -244,27 +317,28 @@ export default {
         this.$q.notify('Please correct the indicated fields.')
       } else {
         try {
+          debugger
           // iOS SAFARI COMPATIBILITY
           let dobTemp = ''
-          try {
-            dobTemp = this.profile.dateOfBirth.substring(0, 10)
-          } catch (e) {
+          if (this.profile.dateOfBirth instanceof Date) {
             dobTemp = this.profile.dateOfBirth.toISOString().substring(0, 10)
+          } else if (typeof this.profile.dateOfBirth === 'string') {
+            dobTemp = this.profile.dateOfBirth.substring(0, 10)
+          } else {
+            dobTemp = this.profile.dateOfBirth
+            console.error(this.profile.dateOfBirth + ' cannot be cut to date only')
           }
           let profile = {
             userKey: userinfo.user._key,
             updatedTS: new Date(),
             name: this.profile.name,
             surname: this.profile.surname,
-            // dateOfBirth: this.profile.dateOfBirth.substring(0, 10),
             dateOfBirth: dobTemp,
             gender: this.profile.gender,
             diseases: this.profile.diseases,
             medications: this.profile.medications,
             lifestyle: this.profile.lifestyle
           }
-          console.log(profile)
-          debugger
           await API.createProfile(profile)
           await userinfo.setProfile(profile)
 
--- src/pages/Home/About.vue
+++ src/pages/Home/About.vue
@@ -1,12 +1,61 @@
 <template>
   <q-page padding class="flex flex-center">
-    <div style="text-align: center">
-      <img src="~assets/oxforduni_logo.png" /><br/><br/>
-      <p>Institute of Biomedical Engineering</p>
-      <p>University of Oxford</p>
-      <p><a href="mailto:ibme-enquries@eng.ox.ac.uk">ibme-enquiries@eng.ox.ac.uk</a></p>
-      <p><a href="https://github.com/Mobistudy">https://github.com/Mobistudy/</a></p>
+    <div class="q-title">
+      Introduction
     </div>
+    <p>
+      Mobistudy is an open iniative that allows citizens to help health research
+      with their data.
+      Participants download an app (iOS or Android) and, after registering and filling-in
+      a short profile, they are proposed with research studies.
+      If they find a study they may be interested in, they are guided through an
+      easy-to-understand informed consent process.
+      Upon acceptance, participants are then proposed "tasks", such as filling forms
+      and questionnaires and provide their health data through Google Fit or Apple HealthKit.
+    </p>
+    <p>
+      Mobistudy is currently managed by the Institute of Biomedical Engineering,
+      University of Oxford.
+    </p>
+    <div class="text-center q-mb-lg">
+      <img src="~assets/oxforduni_logo.png" />
+    </div>
+    <div class="q-title">
+      Terms and conditions
+    </div>
+    <p>
+      This is an alpha version of Mobistudy and is meant to be used for technical
+      testing only. Please do not use this app unless you are a member of the
+      development team of Mobistudy.
+    </p>
+    <div class="q-title">
+      Privacy policy
+    </div>
+    <p>
+      Mobistudy will collect the following information from you, regardless of if
+      have joined or not any research study:
+      <ul>
+        <li>Your email address</li>
+        <li>Your name and surname</li>
+        <li>Your date of bith</li>
+        <li>Your gender</li>
+        <li>Your list of self-declared long-term health conditions</li>
+        <li>Your list of self-declared frequently-used medications</li>
+        <li>Your list of self-declared lifestyle flags (e.g. if you smoke)</li>
+      </ul>
+      These data will stored on a server hosted and managed by the University of Oxford.
+      Personnell from the University of Oxford may access your data for technical maintenance.
+      <br />
+      If you decide to join a research study, further study-specific conditions
+      will apply, which will be clearly shown to you when consenting to participate
+      in the study.
+      <br />
+      Under no circumstances rather than the ones specified above, your data will
+      be shared with other parties or individuals.
+    </p>
+    <p>
+      For any question or doubt, please contact rishi.goburdhun@eng.ox.ac.uk.
+    </p>
   </q-page>
 </template>
 
