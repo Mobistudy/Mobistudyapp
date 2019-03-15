fb9b6e1b86cbebbf14a3989fb5bef0376119ff18
Unknown <arvingob@gmail.com>
2019-03-15 12:07:31 +0000

    Updated userinfo

--- src/modules/userinfo.js
+++ src/modules/userinfo.js
@@ -11,10 +11,11 @@ export default {
         loggedin: false,
         _key: undefined,
         token: undefined,
-        name: undefined,
-        surname: undefined,
-        gender: undefined,
-        dob: undefined
+        // Following commented out for 4YP. To be uncommented after.
+        // name: undefined,
+        // surname: undefined,
+        gender: undefined
+        // dob: undefined
       }
     } else this.user.loggedin = true
   },
@@ -25,9 +26,10 @@ export default {
     await DB.setUserSession(this.user)
   },
   async setProfile (profile) {
-    this.user.name = profile.name
-    this.user.surname = profile.surname
-    this.user.dob = profile.dateOfBirth
+    // Following commented out for 4YP. To be uncommented after.
+    // this.user.name = profile.name
+    // this.user.surname = profile.surname
+    // this.user.dob = profile.dateOfBirth
     this.user.gender = profile.gender
     await DB.setUserSession(this.user)
   },
@@ -36,10 +38,11 @@ export default {
       loggedin: false,
       _key: undefined,
       token: undefined,
-      name: undefined,
-      surname: undefined,
-      gender: undefined,
-      dob: undefined
+      // Following commented out for 4YP. To be uncommented after.
+      // name: undefined,
+      // surname: undefined,
+      gender: undefined
+      // dob: undefined
     }
     DB.removeUserSession()
   }
