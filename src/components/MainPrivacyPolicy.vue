41c4ea22ab0a214dc31ac9c826d1477bce6a78c5
Dario Salvi <dario.salvi.work@gmail.com>
2019-04-03 12:16:53 +0200

    updated general privacy policy

--- src/components/MainPrivacyPolicy.vue
+++ src/components/MainPrivacyPolicy.vue
@@ -19,12 +19,17 @@
     If you decide to join a research study, further study-specific conditions will apply,
     which will be clearly shown to you when consenting to participate in the study.
     <br />
-    Under no circumstances rather than the ones specified above, your data will be shared with other parties or individuals.
+    Under no circumstances rather than the ones specified above,
+    your data will be shared with other parties or individuals.
     <br />
-    You can remove your profile from Mobistudy whenever you want. This will remove all the produced data, except the technical logs.
+    You can remove your profile from Mobistudy whenever you want.
+    This will remove all the produced data, except the technical logs.
     <br />
     You can download the data in a machine-readable format.
     Contact mobistudyOX@gmail.com if you need to.
+    <br />
+    All of your data will be removed after 1 year from your registration
+    or from the end of any study you joined while using the Mobistudy app, whichever is the latest.
   </p>
 </template>
 
