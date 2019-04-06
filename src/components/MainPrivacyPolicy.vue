a40cf548ab9d2b6e1080f66f69ec90bd1531423e
Jameson Lee <jameson.lee@gmx.com>
2019-04-06 15:54:40 +0800

    Fixed about spacing

--- src/components/MainPrivacyPolicy.vue
+++ src/components/MainPrivacyPolicy.vue
@@ -1,36 +1,48 @@
 <template>
-  <p>
-    Mobistudy will collect the following information from you, regardless of if
-    have joined or not any research study:
-    <ul>
-      <li>Your email address.<br>This is needed to identify you, allow you to login and to recover your password.</li>
-      <li>Your date of birth.<br>This is needed to match your profile with the inclusion criteria of available studies.</li>
-      <li>Your gender.<br>This is also needed to match your profile with the inclusion criteria of available studies.</li>
-      <li>Technical logs indicating the time you have accessed the system and what data you have sent.<br> These are needed for security reasons and will be kept for 5 years in all circumstances, also if you decide to delete your account.</li>
-    </ul>
-    These data will be stored on a server hosted and managed by the University of Oxford.
-    Personnel from the University of Oxford may access the server and your data for technical maintenance,
-    but will not download or analyse any of these data.
-    <br />
-    We use Gmail for managing the emails sent by Mobistudy. This means that your email address,
-    together with the content of the email sent by us will be managed by Google according to their
-    Privacy Policy (https://policies.google.com/privacy?hl=en-US).
-    <br />
-    If you decide to join a research study, further study-specific conditions will apply,
-    which will be clearly shown to you when consenting to participate in the study.
-    <br />
-    Under no circumstances rather than the ones specified above,
-    your data will be shared with other parties or individuals.
-    <br />
-    You can remove your profile from Mobistudy whenever you want.
-    This will remove all the produced data, except the technical logs.
-    <br />
-    You can download the data in a machine-readable format.
-    Contact mobistudyOX@gmail.com if you need to.
-    <br />
-    All of your data will be removed after 1 year from your registration
-    or from the end of any study you joined while using the Mobistudy app, whichever is the latest.
-  </p>
+  <div>
+    <p>
+      Mobistudy will collect the following information from you, regardless of if
+      have joined or not any research study:
+      <ul>
+        <li>Your email address.<br>This is needed to identify you, allow you to login and to recover your password.</li>
+        <li>Your date of birth.<br>This is needed to match your profile with the inclusion criteria of available studies.
+        </li>
+        <li>Your gender.<br>This is also needed to match your profile with the inclusion criteria of available studies.</li>
+        <li>Technical logs indicating the time you have accessed the system and what data you have sent.<br> These are
+          needed for security reasons and will be kept for 5 years in all circumstances, also if you decide to delete your
+          account.
+        </li>
+      </ul>
+      These data will be stored on a server hosted and managed by the University of Oxford.
+      Personnel from the University of Oxford may access the server and your data for technical maintenance,
+      but will not download or analyse any of these data.
+    </p>
+    <p>
+      We use Gmail for managing the emails sent by Mobistudy. This means that your email address,
+      together with the content of the email sent by us will be managed by Google according to their
+      Privacy Policy (https://policies.google.com/privacy?hl=en-US).
+    </p>
+    <p>
+      If you decide to join a research study, further study-specific conditions will apply,
+      which will be clearly shown to you when consenting to participate in the study.
+    </p>
+    <p>
+      Under no circumstances rather than the ones specified above,
+      your data will be shared with other parties or individuals.
+    </p>
+    <p>
+      You can remove your profile from Mobistudy whenever you want.
+      This will remove all the produced data, except the technical logs.
+    </p>
+    <p>
+      You can download the data in a machine-readable format.
+      Contact mobistudyOX@gmail.com if you need to.
+    </p>
+    <p>
+      All of your data will be removed after 1 year from your registration
+      or from the end of any study you joined while using the Mobistudy app, whichever is the latest.
+    </p>
+  </div>
 </template>
 
 <script>
