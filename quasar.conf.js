7849449fde47b9936b09cb1ddf1299a487c0c8cf
Dario Salvi <dariosalvi78@gmail.com>
2020-02-12 17:21:43 +0100

    added v-go-back directive, not using it yet though

--- quasar.conf.js
+++ quasar.conf.js
@@ -56,7 +56,8 @@ module.exports = function (ctx) {
         'QTabPanel',
         'QTabs',
         'QTab',
-        'QRouteTab'
+        'QRouteTab',
+        'GoBack'
       ],
       directives: [],
 
