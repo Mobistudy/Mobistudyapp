db0703ba5ce59cbd234e6f0ce8e2efa1ad1e9b45
Felix Morau <felixmorau@gmail.com>
2020-02-18 11:06:18 +0100

    implement folder structure for the six minute walk test

---
+++ src/pages/smwt/smwt.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page padding class="flex flex-center">
+    <div class="text-h6 q-mt-lg">
+      Instructions for the Six Minute Walk Test (6MWT)
+    </div>
+    <q-item class="q-mt-md">
+        <q-item-section>
+          <q-item-label class="q-pb-sm">Introduction</q-item-label>
+          <q-item-label caption>Mobistudy is an open iniative that allows citizens to help health research with their data. Participants download an app (iOS or Android) and, after registering and filling-in a short profile, they are proposed with research studies. If they find a study they may be interested in, they are guided through an easy-to-understand informed consent process. Upon acceptance, participants are then proposed "tasks", such as filling forms and questionnaires and provide their health data through Google Fit or Apple HealthKit.</q-item-label>
+        </q-item-section>
+    </q-item>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'SmwtPage'
+}
+</script>
+
+<style>
+</style>
