2218742e775c1d3b1258823e82f9ed14d0beb7c4
Dario Salvi <dariosalvi78@gmail.com>
2020-02-25 14:36:02 +0100

    geolocation copies the object before sending them to callback

--- src/modules/phone.js
+++ src/modules/phone.js
@@ -36,11 +36,26 @@ export default {
       })
     },
     startNotifications (options, cbk, error) {
-      this.watchid = navigator.geolocation.watchPosition(cbk, error, options)
+      this.watchid = navigator.geolocation.watchPosition((position) => {
+        // we need to create a copy of the position object because
+        // Chromium does something strange that is not serialisable as JSON
+        var copyPos = {}
+        copyPos.timestamp = new Date().getTime() // use current timestamp because some phones mess up the timestamps
+        copyPos.coords = {}
+        copyPos.coords.latitude = position.coords.latitude
+        copyPos.coords.longitude = position.coords.longitude
+        copyPos.coords.altitude = position.coords.altitude
+        if (position.coords.accuracy) copyPos.coords.accuracy = position.coords.accuracy
+        if (position.coords.altitudeAccuracy) copyPos.coords.altitudeAccuracy = position.coords.altitudeAccuracy
+        if (position.coords.heading) copyPos.coords.heading = position.coords.heading
+        if (position.coords.speed) copyPos.coords.speed = position.coords.speed
+
+        cbk(copyPos)
+      }, error, options)
     },
     async stopNotifications () {
       navigator.geolocation.clearWatch(this.watchid)
-      return true
+      return Promise.resolve()
     }
   },
   pedometer: {
