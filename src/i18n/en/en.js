57a4cb5fbc83f9f04b11f3ad3ec5df0fa81fdd06
Dario Salvi <dariosalvi78@gmail.com>
2020-05-06 15:46:58 +0200

    clearer instructions for qcst

--- src/i18n/en/en.js
+++ src/i18n/en/en.js
@@ -252,13 +252,13 @@ export default {
           { p: 'Alternatively, to measure your pulse manually, you can count the number of heart beats for 15 seconds and multiply the count by four.' }
         ],
         instructions: [
-          { i: 'This is a paced test, meaning that you should aim to step using a four-step cadence, "up-up-down-down" for 3 minutes.' },
-          { i: 'When you are ready to start the test, press the "Start"-button.' },
+          { i: 'This is a paced test with a four-step cadence: foot 1 up, foot 2 up, foot 1 down, foot 2 down.' },
           { i: 'Step up and down according to the metronome/pacer. Make sure to turn on the sound on your device and turn up the volume.' },
           { i: 'The test will automaticly stop after 3 minutes. If you need to complete the test earlier, press the "Complete"-button.' },
           { i: 'At the end of the test you will be asked to measure your heart rate and provide it to the app. Please measure your heart rate within 5-20 seconds after completing the test.' },
           { i: 'Try not to talk during the test, as this may affect your performance.' },
-          { i: 'Stop immediately if you have any chest pain or dizziness.' }
+          { i: 'Stop immediately if you have any chest pain or dizziness.' },
+          { i: 'When you are ready to start the test, press the "Start"-button.' }
         ],
         enterHR: 'Enter your heart rate',
         enterHRInstructions: 'Note: To measure your heart rate manually, please count the number of heart beats for 15 seconds. Multiply the count by four and enter the value below.',
