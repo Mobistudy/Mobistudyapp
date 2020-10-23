87023cfecbd59dcd61b2b12723a4de20584535b2
Dario Salvi <dariosalvi78@gmail.com>
2020-10-23 10:37:51 +0200

    using a deep merge for i18n oobjects

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 import en from './en/en'
 import ppEn from './en/privacyPolicy'
 import tcEn from './en/termsAndConditions'
@@ -5,6 +7,24 @@ import tcEn from './en/termsAndConditions'
 import sv from './sv/sv'
 
 export default {
-  'en': Object.assign(en, ppEn, tcEn),
+  'en': mergeDeep(en, ppEn, tcEn),
   'sv': sv
+}
+
+function isObject (item) {
+  return (item && typeof item === 'object' && !Array.isArray(item))
+}
+
+function mergeDeep (target, source) {
+  let output = Object.assign({}, target)
+  if (isObject(target) && isObject(source)) {
+    Object.keys(source).forEach(key => {
+      if (isObject(source[key])) {
+        if (!(key in target)) { Object.assign(output, { [key]: source[key] }) } else { output[key] = mergeDeep(target[key], source[key]) }
+      } else {
+        Object.assign(output, { [key]: source[key] })
+      }
+    })
+  }
+  return output
 }
