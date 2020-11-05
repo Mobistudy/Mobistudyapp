ef55383d66656ce6c225c0ad57ae060bfbefd2da
Dario Salvi <dariosalvi78@gmail.com>
2020-11-05 20:24:23 +0100

    improved styling of menu

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,6 +1,10 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header reveal elevated class="bg-primary text-white">
+    <q-header
+      reveal
+      elevated
+      class="bg-primary text-white"
+    >
       <q-toolbar>
 
         <q-toolbar-title>
@@ -8,13 +12,42 @@
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
-    <q-footer bordered class="elevated">
-      <q-tabs mobile-arrows narrow-indicator dense active-color="secondary" class="bg-white text-black">
-        <q-route-tab class="q-px-sm" to="tasker" icon="check_box">{{ $t('layouts.homeMenu.dailyTasks') }}</q-route-tab>
-        <q-route-tab class="q-px-sm" to="profile" icon="account_box" >{{ $t('layouts.homeMenu.profile') }}</q-route-tab>
-        <q-route-tab class="q-px-sm" to="studies" icon="settings">{{ $t('layouts.homeMenu.studies') }}</q-route-tab>
-        <q-route-tab class="q-px-sm" to="about" icon="help">{{ $t('layouts.homeMenu.about') }}</q-route-tab>
-        <q-route-tab class="q-px-sm" to="test" icon="bug_report">TEST</q-route-tab>
+    <q-footer
+      bordered
+      class="elevated"
+    >
+      <q-tabs
+        mobile-arrows
+        narrow-indicator
+        dense
+        active-color="secondary"
+        class="bg-white text-grey-7"
+      >
+        <q-route-tab
+          class="q-px-sm"
+          to="tasker"
+          icon="check_box"
+        >{{ $t('layouts.homeMenu.dailyTasks') }}</q-route-tab>
+        <q-route-tab
+          class="q-px-sm"
+          to="profile"
+          icon="account_box"
+        >{{ $t('layouts.homeMenu.profile') }}</q-route-tab>
+        <q-route-tab
+          class="q-px-sm"
+          to="studies"
+          icon="settings"
+        >{{ $t('layouts.homeMenu.studies') }}</q-route-tab>
+        <q-route-tab
+          class="q-px-sm"
+          to="about"
+          icon="help"
+        >{{ $t('layouts.homeMenu.about') }}</q-route-tab>
+        <q-route-tab
+          class="q-px-sm"
+          to="test"
+          icon="bug_report"
+        >TEST</q-route-tab>
       </q-tabs>
     </q-footer>
     <q-page-container>
