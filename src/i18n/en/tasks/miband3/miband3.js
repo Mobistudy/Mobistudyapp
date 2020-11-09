04bf86729ab72739a9b98a55af1c7386af5c8c06
Dario Salvi <dariosalvi78@gmail.com>
2020-11-09 20:14:10 +0100

    typo

--- src/i18n/en/tasks/miband3/miband3.js
+++ src/i18n/en/tasks/miband3/miband3.js
@@ -13,7 +13,7 @@ export default {
           unknown: 'Unknown'
         },
         dataDownloadError: 'Could not retrieve data from the band, please retry or cancel.',
-        chartsIntro: 'The following charts summarize the data that has been retrieved from the band. Tap on "Send" to share these data with the research team or tap on "Skip" to avoid sending these data.',
+        chartsIntro: 'The following charts summarize the data that have been retrieved from the band. Tap on "Send" to share these data with the research team or tap on "Skip" to avoid sending these data.',
         lineChart: 'Activity over time',
         hrs: 'heart rate',
         intensities: 'intensity',
