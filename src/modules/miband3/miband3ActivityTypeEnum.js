1fa200cf4f99b26b2a4fb62ff7380465548f60aa
Dario Salvi <dariosalvi78@gmail.com>
2021-01-18 14:16:03 +0100

    more miband3 activities mapped

--- src/modules/miband3/miband3ActivityTypeEnum.js
+++ src/modules/miband3/miband3ActivityTypeEnum.js
@@ -6,13 +6,29 @@
 export function getStringIdentifier (index) {
   switch (index) {
     case 1:
+    case 16:
       return 'walk'
+    case 3:
+      return 'not_worn'
     case 6:
       return 'charging'
+    case 80:
+    case 90:
+    case 89:
+    case 91:
+    case 92:
+    case 96:
+      return 'sedentary'
+    case 98:
+    case 82:
+      return 'running'
     case 17:
       return 'activity_high'
+    case 106:
     case 112:
+    case 121:
     case 122:
+    case 123:
       return 'sleep'
     default:
       return 'unknown'
