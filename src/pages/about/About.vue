e3175aef56e99224ce738689ad4d88e52806821d
Dario Salvi <dariosalvi78@gmail.com>
2021-02-10 00:00:44 +0100

    about page without buttons

--- src/pages/about/About.vue
+++ src/pages/about/About.vue
@@ -2,16 +2,16 @@
   <q-page padding>
     <div align="center">
       <div class="q-pa-md">
-        <q-btn to='/introduction' style='width: 200px' :label="$t('about.introduction')" color='primary'/>
+        <q-btn flat to='/introduction' style='width: 200px' :label="$t('about.introduction')" color='primary'/>
       </div>
       <div class="q-pa-md">
-        <q-btn to='/aboutPrivacyPolicy' style='width: 200px' :label="$t('about.privacyPolicy')" color='primary' />
+        <q-btn flat to='/aboutPrivacyPolicy' style='width: 200px' :label="$t('about.privacyPolicy')" color='primary' />
         </div>
       <div class="q-pa-md">
-        <q-btn to='/acknowledgements' style='width: 200px' :label="$t('about.acknowledgements')" color='primary' />
+        <q-btn flat to='/acknowledgements' style='width: 200px' :label="$t('about.acknowledgements')" color='primary' />
       </div>
        <div class="q-pa-md">
-        <q-btn to='/contact' style='width: 200px' :label="$t('about.contact')" color='primary' />
+        <q-btn flat to='/contact' style='width: 200px' :label="$t('about.contact')" color='primary' />
       </div>
     </div>
   </q-page>
