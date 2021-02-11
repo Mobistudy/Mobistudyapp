964c30c963d7348f04b90bff1af9a2396447aa99
Dario Salvi <dariosalvi78@gmail.com>
2021-02-11 20:45:47 +0100

    improved acks section

--- src/i18n/sv/about/acks.mjs
+++ src/i18n/sv/about/acks.mjs
@@ -3,9 +3,12 @@ export default `Mobistudy was started at the University of Oxford through the fo
 <li><a href="https://oxfordbrc.nihr.ac.uk/">NIHR Oxford Biomedical Research Centre (BRC)</a></li>
 <li><a href="https://www.clahrc-oxford.nihr.ac.uk/">NIHR Oxford Collaboration for Leadership in Applied Health Research (CLAHRC)</a></li>
 </ul>
-It is currently beign developed at Malmö University and funded by the \
-Knowledge Foundation through the \
-<a href="https://mau.se/en/research/research-centres/internet-of-things-and-people/">Internet of Things and People</a> research profile.
+It is currently beign developed at Malmö University and funded by: \
+<ul>
+<li>The Knowledge Foundation through the \
+<a href="https://mau.se/en/research/research-centres/internet-of-things-and-people/">Internet of Things and People</a> research profile</li>
+<li>The <a href="https://www.matspaulssonstiftelserna.com/">Mats Paulsson Foundation</a></li>
+</ul>
 <br>
 <br>
 The project was started by Dario Salvi, Carmelo Velardo and Rishi Arvin Goburdhun at Oxford \
@@ -14,8 +17,8 @@ and is currently coordinated by Dario Salvi and Carl Magnus Olsson at Malmö.
 <br>
 The following people have contributed to the project with code, design or ideas:
 <ul>
-<li><a href="https://github.com/jamtholee">Jameson Lee</a></li>
 <li><a href="https://github.com/assimilate">Daniel Abella</a></li>
+<li><a href="https://github.com/jamtholee">Jameson Lee</a></li>
 <li><a href="https://github.com/lencz">Lennart Czienskowski</a></li>
 <li><a href="https://github.com/elinforsnor">Elin Forsnor</a></li>
 <li><a href="https://github.com/femosc2">Felix Morau</a></li>
@@ -26,4 +29,28 @@ The following people have contributed to the project with code, design or ideas:
 <li><a href="https://github.com/JohnHakansson">John Håkansson</a></li>
 <li>Matilda Svensson</li>
 </ul>
+<br>
+<br>
+The following libraries and frameworks have been used to develop this app:
+<ul>
+<li><a href="https://cordova.apache.org/">Apache Cordova</a></li>
+<li><a href="https://vuejs.org/">Vue JS</a></li>
+<li><a href="https://quasar.dev/">Quasar dev</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-badge">cordova-plugin-badge</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-device">cordova-plugin-device</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-ble-central">cordova-plugin-ble-central</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-file">cordova-plugin-file</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-geolocation">cordova-plugin-geolocation</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-health">cordova-plugin-health</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-insomnia">cordova-plugin-insomnia</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-local-notification">cordova-plugin-local-notification</a></li>
+<li><a href="https://github.com/dariosalvi78/cordova-plugin-pedometer">cordova-plugin-pedometer</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-pincheck">cordova-plugin-pincheck</a></li>
+<li><a href="https://github.com/mibrito707/cordova-plugin-secure-storage-echo/">cordova-plugin-cordova-plugin-secure-storage-echo</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-splashscreen">cordova-plugin-splashscreen</a></li>
+<li><a href="https://www.npmjs.com/package/cordova-plugin-whitelist">cordova-plugin-whitelist</a></li>
+</ul>
+<br>
+<br>
+Icons made by itim2101 from www.flaticon.com.
 `
