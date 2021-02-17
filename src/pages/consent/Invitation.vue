863649039ccd2d5f11749b1f664bd84250d7b7ab
Dario Salvi <dariosalvi78@gmail.com>
2021-02-17 15:48:26 +0100

    show invitation letter not PP

--- src/pages/consent/Invitation.vue
+++ src/pages/consent/Invitation.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-    <div class="q-my-md" style="white-space: pre-line;">{{ studyDescription.consent.privacyPolicy[$i18n.locale] }}</div>
+    <div class="q-my-md" style="white-space: pre-line;">{{ studyDescription.consent.invitation[$i18n.locale] }}</div>
 
     <div class="q-my-md row justify-around">
       <q-btn class="q-mt-lg full-width" :label="$t('common.next')" color="primary" @click="next()"></q-btn>
