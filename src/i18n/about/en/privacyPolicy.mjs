148281968ed41ea53e951014c9d82ddb21b33551
Dario Salvi <dariosalvi78@gmail.com>
2021-02-25 17:23:45 +0100

    updated general privacy policy

--- src/i18n/about/en/privacyPolicy.mjs
+++ src/i18n/about/en/privacyPolicy.mjs
@@ -1,85 +1,123 @@
-export default `<p>Version 2020-06-18</p>
+export default `<h1>Privacy policy of the Mobistudy participants app</h1>
+  <h2>Version 2021-02-25</h2>
+
   <p>
-    The Mobistudy app is meant to be used by citizens who want to contribute to
-    research studies using their mobile phones.
-    In order to use the app you will need to create an account on the Mobistudy
-    server. This profile is needed regardless of whether you join a
-    research study or not. The profile requires the following information:
+    The Mobistudy app is meant to be used by citizens who want to contribute to research studies using their mobile
+    phones.
+    In order to use the app you will need to create an account on the app which will be shared with the Mobistudy
+    server.
+    This profile is needed regardless of whether you join a research study or not. The profile requires the following
+    information:
+  </p>
+
+  <ul>
+    <li>
+      Your email address. This is needed to identify you, allow you to login, and to recover your password.
+    </li>
+    <li>
+      Your name and surname. These are needed to personalise messages sent to you or in those studies where it is
+      necessary to identify each participant.
+    </li>
+    <li>
+      Your date of birth. This is needed to match your profile with the inclusion criteria of available studies.
+    </li>
+    <li>
+      Your sex. This is also needed to match your profile with the inclusion criteria of available studies.
+    </li>
+    <li>
+      Any long term conditions, like asthma or diabetes. This is needed to match your profile with the inclusion
+      criteria of available studies.
+    </li>
+    <li>
+      Any medication that you take regularly, for example insulin. This is needed to match your profile with the
+      inclusion criteria of available studies.
+    </li>
+    <li>
+      Your height and weight. These are needed to match inclusion criteria and to configure some of the activities you
+      can perform using the app.
+    </li>
+  </ul>
+
+  <p>
+    In addition to the data contained in this profile, the app will collect and share information about your
+    participation in research studies with the Mobistudy server, like the date and time you joined or withdrew from a
+    study, or the date and time of the activities you performed within a study. These are needed to keep track of your
+    participation in existing studies and avoid inviting you to studies you are already taking part in, or studies that
+    are not suitable to be part of at the same time.
+  </p>
+
+  <p>
+    If you decide to join a research study, you may be required to share your data with the institutions involved in the
+    study, including:
   </p>
   <ul>
     <li>
-      Your email address.
-      This is needed to identify you, allow you to login, and to recover your password.
+      Answers given to forms and questionnaires.
     </li>
     <li>
-      Your name and surname.
-      These may be needed to personalise messages sent to you or in those studies
-      where it will be necessary to identify each participant.
+      Data collected from Google Fit (Android phones), or HealthKit (iPhones). Only the data of relevance to each study
+      will be collected from Google Fit or Health Kit. This may include your weight, height, steps count, heart rate,
+      distance, activity, sleep, or nutrition.
     </li>
     <li>
-      Your date of birth.
-      This is needed to match your profile with the inclusion criteria of available studies.
+      Heart rate and steps collected during a Queens College Step Test.
     </li>
     <li>
-      Your sex.
-      This is also needed to match your profile with the inclusion criteria of available studies.
+      Geographical position, distance and steps during the 6-minute Walk Test.
     </li>
     <li>
-      Any long term conditions, like asthma or diabetes.
-      This is needed to match your profile with the inclusion criteria of available studies.
+      Heart rate, activity level, steps and activity type collected by the MiBand3 fitness tracker.
     </li>
     <li>
-      Any medication that you take regularly, for example insulin.
-      This is needed to match your profile with the inclusion criteria of available studies.
+      Blood oxygen saturation and heart rate measured by a pulseoximeter.
     </li>
   </ul>
+
   <p>
-    In addition to the data contained in this profile, the app will share information about your 
-    participation to research studies with the Mobistudy server, like the date and time you 
-    joined or withdrew from a study, or the date and time of the activities you performed within 
-    a study. These are needed to keep track of your participation in existing studies and
-    avoid inviting you to studies you are already taking part in, or studies that are not suitable
-    to be part of at the same time.
+    The purpose and extent of data collection within each study will depend on the study itself. A further
+    study-specific privacy policy will be clearly shown to you before joining the study and you will be allowed to
+    accept or reject it.
   </p>
+
   <p>
-    These data are securely transferred (via HTTPS) and stored on a server
-    hosted and managed by the University of Malmö, Sweden. Data is encrypted on 
-    both the app and the server and during transmission (via https). Personnel 
-    from the University of Malmö may access the server and your data for 
-    technical maintenance, but will not analyse any of these data, unless
-    they are part of the research study you have decided to participate in.
+    All the collected data are securely transferred and stored on a server hosted and managed by the University of
+    Malmö, Sweden. Data is encrypted on both the app and the server and during transmission. Personnel from the
+    University of Malmö may access the server and your data for technical maintenance, but will not process any of these
+    data for clinical or research purposes, unless they are part of the research studies you have decided to participate
+    in. The University of Malmö may publish general statistics about the number of users registered on the platform,
+    their country of residence and age group.
   </p>
+
   <p>
-    If you decide to join a research study, you may be required to share your data
-    with the institutions involved in the study, but further study-specific
-    conditions will apply, which will be clearly shown to you and you will be allowed
-    to accept or reject.
+    Under no circumstances other than the ones specified above, will your data be shared with other parties or
+    individuals.
   </p>
+
   <p>
-    Under no circumstances other than the ones specified above, will your data be
-    shared with other parties or individuals.
+    Your data are kept on the server for as long as you decide to keep your profile on Mobistudy. Study specific-data
+    will be kept for the duration of study, which is going to be specified when you join a study. The app allows you to
+    delete your account and any data associated with it at any time, by tapping a button.
   </p>
+
   <p>
-    Your data are kept on the server for as long as you decide to keep your profile on
-    Mobistudy. Study specific-data will be kept for the duration of study, which
-    is going to be specified when you join a study. The app allows you to delete your 
-    account and any data associated with it at any time, by tapping a button.
+    The server will also record technical logs containing the date and time you send or request data and the IP address
+    from which the data comes from, but not the data associated with the request. These technical logs are needed for
+    security audit and will be kept for 1 year in all circumstances, also if you decide to delete your account.
   </p>
+
   <p>
-    The server will register the date and time you login or send data and
-    the IP address from which the data comes from. These technical logs are needed 
-    to allow for security audits and will be kept for 5 years in all circumstances, 
-    even if you decide to delete your account.
+    You can download the data in a machine-readable format. Contact mobistudy@mau.se if you need to do this.
   </p>
+
   <p>
-    You can download the data in a machine-readable format. Contact mobistudy@mau.se
-    if you need to do this.
+    Malmö University is the Controller of the processing of personal information which is done based on your consent.
+    The consent can be revoked at any time by contacting mobistudy@mau.se and any material processed based on your
+    previous consent will be erased. The same address applies if you would like to find out what has been registered
+    about you or if you would like to have errors corrected, information erased or processing restricted.
   </p>
+
   <p>
-    The <a href="https://mau.se/en/about-the-website/">general privacy policy</a>
-    of the University of Malmö applies for cases not explicitly mentioned in this 
-    privacy policy, for example for what regards communication via email with 
-    staff managing Mobistudy. For enquiries, you can contact the Mobistudy team 
-    at mobistudy@mau.se or the University's Data Protection Officer at 
-    dataskyddsombud@mau.se
+    If you have questions about the university’s processing of personal information you may contact the Data Protection
+    Officer at dataskyddsombud@mau.se and if you have complaints that cannot be resolved with Malmö university you may
+    submit these to Datainspektionen.
   </p>`
