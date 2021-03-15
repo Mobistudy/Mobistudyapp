922a7c88fb0ccf7128f53fe96f6fd275ea84b750
Dario Salvi <dariosalvi78@gmail.com>
2021-03-15 20:41:10 +0100

    renaming driver to cordova

--- project.config.template.js
+++ project.config.template.js
@@ -6,6 +6,6 @@ module.exports = {
   NOTIFICATIONS: 'cordova', // use 'WEB' for browser notifications or 'cordova' for the cordova plugin
   STORAGE: 'encrypted', // use 'local' for browser localStorage or 'native' for cordova native storage, 'encrypted' for encrypted native
   PHONE: 'cordova', // use 'MOCK' for mocked phone functionalities or 'cordova' otherwise
-  MIBAND3: 'driver', // use 'MOCK' for mocked tracker or 'driver' otherwise
-  PO60: 'driver' // use 'MOCK' for mocked pulse oximeter or 'driver' otherwise
+  MIBAND3: 'cordova', // use 'MOCK' for mocked tracker or 'cordova' otherwise
+  PO60: 'cordova' // use 'MOCK' for mocked pulse oximeter or 'cordova' otherwise
 }
