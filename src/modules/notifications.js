53f5389d5e84deff40963be366de619455160932
Dario Salvi <dariosalvi78@gmail.com>
2021-03-18 10:24:35 +0100

    really try to get permissions for notifications

--- src/modules/notifications.js
+++ src/modules/notifications.js
@@ -1,12 +1,9 @@
-import { Platform } from 'quasar'
 
 export default {
   async hasPermission () {
-    // has permission does not make sense on Android (android does not need permissions) and does not work on iOS
-    // so we'll return immediately that we do not have permission if on iOS and
-    // that we do have permission on Android
-    if (Platform.is.android) return Promise.resolve(true)
-    else return Promise.resolve(false)
+    return new Promise((resolve, reject) => {
+      cordova.plugins.notification.local.hasPermission(resolve)
+    })
   },
   async requestPermission () {
     return new Promise((resolve, reject) => {
