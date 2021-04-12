99cd51132860dc51ea2c7005ff3ada50dcfe3046
Carl <carl.magnus.olsson@mau.se>
2021-04-12 16:17:22 +0200

    Intro translation screens to Swedish.

--- src/i18n/accountMgmt/welcomeIntro.mjs
+++ src/i18n/accountMgmt/welcomeIntro.mjs
@@ -1,14 +1,14 @@
 export default {
   en: {
     intro1: 'Welcome to Mobistudy! This app allows you to participate to clinical research using your phone.',
-    intro2: 'Researchers around the world create studies which are proposed to you based on your profile.',
+    intro2: 'Researchers around the world create studies which are compared with your profile to determine if you suit their studies.',
     intro3: 'You can contribute to the research by collecting data in “tasks” and by sharing that data with the researchers.',
     intro4: 'All your data is kept secure and you will have complete control over what is shared and when.'
   },
   sv: {
-    intro1: 'CARL',
-    intro2: 'CARL',
-    intro3: 'CARL',
-    intro4: 'CARL'
+    intro1: 'Välkommen till Mobistudy! Denna app låter dig delta i klinisk forskning genom din telefon.',
+    intro2: 'Forskare världen över skapar studier som jämförs med din profil för att avgöra om du passar för deras studier.',
+    intro3: 'Du kan bidra till forskning genom att samla in data via "uppgifter" och genom att dela data med forskarna.',
+    intro4: 'All din data lagras på ett säkert sätt och du har fullständig kontroll över vad som delas och när.'
   }
 }
