40ab8acba3ec1d876ae11c5f167cafb85ea7bbaa
Dario Salvi <dariosalvi78@gmail.com>
2021-04-20 11:15:34 +0200

    moved welcome intro i18n to their folders

---
+++ src/i18n/accountMgmt/en/welcomeIntro.mjs
@@ -0,0 +1,6 @@
+export default {
+  intro1: 'Welcome to Mobistudy! This app allows you to participate to clinical research using your phone.',
+  intro2: 'Researchers around the world create studies which are compared with your profile to determine if you suit their studies.',
+  intro3: 'You can contribute to the research by collecting data in “tasks” and by sharing that data with the researchers.',
+  intro4: 'All your data is kept secure and you will have complete control over what is shared and when.'
+}
