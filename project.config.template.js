45792a9d17997775cb12ce8fa24fb7e81c3d992a
Kevin Tsang <51415694+kevinchtsang@users.noreply.github.com>
2021-04-23 15:35:08 +0100

    Update project.config.template.js

--- project.config.template.js
+++ project.config.template.js
@@ -1,12 +1,27 @@
 
 // USE THESE TO TWEAK THE COMPILATION OPTIONS
 module.exports = {
-  API_ENDPOINT: 'https://app.mobistudy.org/api', // use 'MOCK' for mock api, '/api' for local server
-  HEALTHSTORE: 'cordova', // use 'MOCK' for mock healthstore or 'cordova' for the cordova health plugin
-  NOTIFICATIONS: 'cordova', // use 'WEB' for browser notifications or 'cordova' for the cordova plugin
-  STORAGE: 'encrypted', // use 'local' for browser localStorage or 'native' for cordova native storage, 'encrypted' for encrypted native
-  PHONE: 'cordova', // use 'MOCK' for mocked phone functionalities or 'cordova' otherwise
-  MIBAND3: 'cordova', // use 'MOCK' for mocked tracker or 'cordova' otherwise
-  PO60: 'cordova', // use 'MOCK' for mocked pulse oximeter or 'cordova' otherwise
-  PEAKFLOW: 'MOCK' // use 'MOCK' for mocked peak flow meter or 'cordova' otherwise
+  // use 'MOCK' for mock api, '/api' for local server, 'https://app.mobistudy.org/api' for real server
+  API_ENDPOINT: 'https://app.mobistudy.org/api',
+
+  // use 'MOCK' for mock healthstore or 'cordova' for the cordova health plugin
+  HEALTHSTORE: 'cordova',
+
+  // use 'MOCK' for mock , 'WEB' for browser notifications or 'cordova' for the cordova plugin
+  NOTIFICATIONS: 'cordova',
+
+  // use 'MOCK' for mock, 'local' for browser localStorage or 'encrypted' for encrypted native
+  STORAGE: 'encrypted',
+
+  // use 'MOCK' for mocked phone functionalities or 'cordova' otherwise
+  PHONE: 'cordova',
+
+  // use 'MOCK' for mocked tracker or 'cordova' otherwise
+  MIBAND3: 'cordova',
+
+  // use 'MOCK' for mocked pulse oximeter or 'cordova' otherwise
+  PO60: 'cordova',
+
+  // use 'MOCK' for mocked pulse oximeter or 'cordova' otherwise
+  PEAKFLOW: 'MOCK'
 }
