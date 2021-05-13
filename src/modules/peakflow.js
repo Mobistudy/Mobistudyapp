c3708d5078c2a398b38d45a858063865d75ad047
Kevin Tsang <51415694+kevinchtsang@users.noreply.github.com>
2021-05-13 17:57:07 +0100

    add cordova-plugin-spf

--- src/modules/peakflow.js
+++ src/modules/peakflow.js
@@ -1,17 +1,35 @@
 'use strict'
 
+import spf from 'cordova-plugin-spf'
+
 // smart peak flow meter cordova-plugin
 export default {
   async startCalibration () {
+    return new Promise((resolve, reject) => {
+      if (!spf) reject(new Error('Cordova spf is not installed'))
+      spf.startCalibration(resolve, reject)
+    })
   },
 
   async stopCalibration () {
+    return new Promise((resolve, reject) => {
+      if (!spf) reject(new Error('Cordova spf is not installed'))
+      spf.stopCalibration(resolve, reject)
+    })
   },
 
   async startMeasurement () {
+    return new Promise((resolve, reject) => {
+      if (!spf) reject(new Error('Cordova spf is not installed'))
+      spf.startMeasurement(resolve, reject)
+    })
   },
 
   async stopMeasurement () {
+    return new Promise((resolve, reject) => {
+      if (!spf) reject(new Error('Cordova spf is not installed'))
+      spf.stopMeasurement(resolve, reject)
+    })
   },
 
   async getStoredData (startDate) {
