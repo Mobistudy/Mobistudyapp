48bc2e5f186ba898af433555381f5b216a6b2f38
Kevin Tsang <51415694+kevinchtsang@users.noreply.github.com>
2021-05-17 17:01:37 +0100

    add Peak flow Rityta images

--- src/i18n/studies/en/tasks/peakflow.mjs
+++ src/i18n/studies/en/tasks/peakflow.mjs
@@ -6,24 +6,24 @@ export default {
   shortDescription: 'Record peak flow with smart peak flow meter',
   introductionSlides: [
     {
-      title: 'Requirements',
-      // img: '/statics/instructions/qcst_1.jpg',
-      description: 'For this task you will need to connect the smart peak flow meter to your phone, either with the Bluetooth adpater or with the audio jack.'
+      // title: 'Requirements',
+      img: 'instructions/peakflow_1.svg',
+      description: 'In this task you are going to measure your peak flow, which is a measure of how quickly you can blow air out of your lungs.'
     },
     {
       title: 'Requirements',
-      // img: '/statics/instructions/qcst_1.jpg',
-      description: 'You will need a light source for the peak flow meter, either a desk lamp or the window would suffice.'
+      img: 'instructions/peakflow_2.svg',
+      description: 'For this task, you need the “Smart Peak Flow” meter. Plug it into the phone audio socket. If you phone does not have an audio jack, use the Bluetooth adapter instead.'
     },
     {
       title: 'Setup',
-      // img: '/statics/instructions/qcst_1.jpg',
-      description: `Don't cover the window at the top of the device.`
+      img: 'instructions/peakflow_3.svg',
+      description: `Your device requires light form above, like sunlight or an electric bulb. Make sure you are not covering the top of the device.`
     },
     {
       title: 'Instruction',
-      // img: '/statics/instructions/qcst_1.jpg',
-      description: 'Stand upright and tap start, then ... 1) inhale as deeply as you can 2) cover the whole device with your mouth 3) exhale hard through your mouth. Repeat 3 times'
+      img: 'instructions/peakflow_4.svg',
+      description: 'Stand up and tap on “Start”. Then 1) inhale as much as you can, 2) cover the whole device with your mouth, 3) exhale as hard as you can. This is repeated 3 times.'
     }
   ],
   pef: 'Peak Flow',
