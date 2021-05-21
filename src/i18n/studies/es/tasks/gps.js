d1023d035a8f0452fab8a7ebff865217de85f57c
Kevin Tsang <51415694+kevinchtsang@users.noreply.github.com>
2021-05-21 15:34:23 +0100

    clean up weather task interface

--- src/i18n/studies/es/tasks/gps.js
+++ src/i18n/studies/es/tasks/gps.js
@@ -7,7 +7,7 @@ export default {
   OSpermissionAndroid: 'This task requires the app to access the positioning system of your phone (like the GPS). Your location will be shared with the research team of this study for analysis. Tap on Next if you want to proceed with the authorization process.',
   introductionSlides: [
     {
-      title: 'Instruction',
+      title: '',
       img: 'instructions/6mwt_6.jpg',
       description: 'We will use your location to find out your local weather.'
     },
