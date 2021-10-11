86ef9896642850452585d353a6e0f51d271ddda2
Dario Salvi <dariosalvi78@gmail.com>
2021-10-11 14:46:27 +0200

    added finger tapping intro images

--- src/i18n/studies/en/tasks/tapping.js
+++ src/i18n/studies/en/tasks/tapping.js
@@ -5,12 +5,12 @@ export default {
   introductionSlides: [
     {
       title: 'Instruction',
-      img: 'instructions/fingertapping_1.png',
+      img: 'instructions/fingertapping1.svg',
       description: 'In this task you will have to tap the screen of your phone with your index and middle fingers as fast as you can.'
     },
     {
       title: 'Setup',
-      img: 'instructions/fingertapping_1.png',
+      img: 'instructions/fingertapping2.svg',
       description: `Make sure your phone is flat on a surface before the test begins.
       Then use the index and middle fingers of the dominant hand to alternately tap the buttons with numbers 1 and 2.
       Keep tapping for 20 seconds and try to be as regular as possible.`
--- src/i18n/studies/es/tasks/tapping.js
+++ src/i18n/studies/es/tasks/tapping.js
@@ -5,12 +5,12 @@ export default {
   introductionSlides: [
     {
       title: 'Instrucciones',
-      img: 'instructions/fingertapping_1.png',
+      img: 'instructions/fingertapping1.svg',
       description: 'En esta tarea tendrás que tocar la pantalla de tu teléfono con tus dedos índice y medio tan rápido como puedas.'
     },
     {
       title: 'Configuración',
-      img: 'instructions/fingertapping_1.png',
+      img: 'instructions/fingertapping2.svg',
       description: `Asegúrate de que el teléfono esté apoyado sobre una superficie plana antes de que comience la prueba.
       Luego, usa los dedos índice y medio de la mano dominante para tocar alternativamente los botones con los números 1 y 2.
       Sigue pulsando durante 20 segundos e intenta ser lo más regular posible.`
