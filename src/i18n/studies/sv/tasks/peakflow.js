7770c22779ebf053c63f2f58d149af353b29578a
Carl <carl.magnus.olsson@mau.se>
2021-10-11 16:27:44 +0200

    Peakflow and tapping translations to Swedish

--- src/i18n/studies/sv/tasks/peakflow.js
+++ src/i18n/studies/sv/tasks/peakflow.js
@@ -1,52 +1,52 @@
 export default {
-  title: 'Smart Peak Flow Meter measurement',
-  shortTitle: 'Peak Flow',
-  shortDescription: 'Record peak flow with the smart peak flow meter',
-  OSpermissioniOS: `This task requires the app to access the microphone on your phone.
-  This is needed to measure peak flow using the Smart Peak Flow Meter.
-  The collected data will be shared with the research team of this study for analysis.
-  Tap on Next if you want to proceed with the authorization process.`,
-  OSpermissionAndroid: `This task requires the app to access the microphone on your phone.
-  This is needed to measure peak flow using the Smart Peak Flow Meter.
-  The collected data will be shared with the research team of this study for analysis.
-  Tap on Next if you want to proceed with the authorization process.`,
+  title: 'Smart toppflödesmätning',
+  shortTitle: 'Toppflöde',
+  shortDescription: 'Mät toppflöde på din utandning med den smarta PEF-mätaren',
+  OSpermissioniOS: `Denna uppgift kräver att appen har behörighet att använda mikrofonen på din telefon. 
+  Detta behövs för att kunna mäta toppflödet med den smarta PEF-mätaren.
+  För analys delas insamlad data med forskningsgruppen för denna studie.
+  Klicka på Nästa om du vill fortsätta med behörighetsprocessen.`,
+  OSpermissionAndroid: `Denna uppgift kräver att appen har behörighet att använda mikrofonen på din telefon. 
+  Detta behövs för att kunna mäta toppflödet med den smarta PEF-mätaren.
+  För analys delas insamlad data med forskningsgruppen för denna studie.
+  Klicka på Nästa om du vill fortsätta med behörighetsprocessen.`,
   introductionSlides: [
     {
       // title: 'Requirements',
       img: 'instructions/peakflow_1.svg',
-      description: 'In this task you are going to measure your peak flow, which is a measure of how quickly you can blow air out of your lungs.'
+      description: 'I denna uppgift mäts ditt toppflöde, dvs hur fort du kan blåsa ut luften ur dina lungor.'
     },
     {
-      title: 'Requirements',
+      title: 'Krav',
       img: 'instructions/peakflow_2.svg',
-      description: 'For this task, you need to plug the “Smart Peak Flow” meter into the audio socket of your phone. If your phone does not have an audio jack, use the Bluetooth adapter instead.'
+      description: 'För att göra denna uppgift måste du koppla in din smarta PEF-mätare till ljuduttaget på din telefon. Om din telefon saknar en sådan kontakt, använd Bluetooth-adaptern istället.'
     },
     {
-      title: 'Setup',
+      title: 'Förberedelser',
       img: 'instructions/peakflow_3.svg',
-      description: `The peak flow meter requires light form above, like sunlight or an electric bulb. Make sure you are not covering the top of the device.`
+      description: `Toppflödesmätaren behöver ljus uppifrån, såsom solljus eller från en lampa. Försäkra dig om att du inte täcker för ovansidan av toppflödesmätaren.`
     },
     {
-      title: 'Instruction',
+      title: 'Instruktioner',
       img: 'instructions/peakflow_4.svg',
-      description: 'When ready, stand up and tap on “Start”. Then 1) inhale as much as you can, 2) cover the whole device with your mouth, 3) exhale as hard as you can. This is repeated 3 times.'
+      description: 'När du är redo, ställ dig upp och tryck på Starta. Därefter, (1) andas in så mycket du kan, (2) täck flödesmätaren med din mun, samt (3) andas ut så fort du kan. Gör om dessa steg tre gånger.'
     }
   ],
-  calibration: 'Calibration',
-  calibrationStart: 'Connect your Smart Peak Flow meter to the phone and hold your phone under a light source. Press "Start" when ready.',
-  calibrating: 'Calibrating...',
-  calibrationError: 'There was an error during calibration. Please check the top of the device is not covered, the connection between the peak flow meter and your phone/Bluetooth adapter is secure, and that there is sufficient light above the peak flow meter.',
-  measurement: 'Measurement',
+  calibration: 'Kalibrering',
+  calibrationStart: 'Anslut din smarta PEF-mätare till din telefon och håll din telefon under en ljuskälla. Tryck på Starta när du är redo.',
+  calibrating: 'Kalibrerar...',
+  calibrationError: 'Ett fel uppstod under kalibreringen. Vänligen kontrollera så att ovansidan av din PEF-mätare inte är täckt, att anslutningen mellan PEF-mätaren och din telefon/Bluetooth-adapter är stabil, samt att det finns tillräckligt med ljus ovanför mätaren.',
+  measurement: 'Mätning',
   summary: 'Översikt',
-  measurementStart: 'Stay still and make sure you are under a light source. When ready, press "Start".',
-  measurementInstructions: 'Inhale as much as you can, cover the device with your mouth and exhale as hard as you can.',
-  measurementCompleted1: 'First measurement completed. Please press "Start" when ready to start another measurement.',
-  measurementCompleted2: 'Second measurement completed. Please press "Start" when ready to start another measurement.',
-  measurementCompleted3: 'Third and last measurement completed. Press "Next" to proceed.',
-  measurementError: 'Något gick fel, försök igen',
+  measurementStart: 'Var stilla och försäkra dig om att du är under en ljuskälla. När du är redo, tryck på Starta.',
+  measurementInstructions: 'Andas in så mycket du kan, täck mätaren med din mun, samt andas ut så fort du kan.',
+  measurementCompleted1: 'Första mätningen är klar. Tryck på Starta när du är redo att göra en till mätning.',
+  measurementCompleted2: 'Andra mätningen är klar. Tryck på Starta när du är redo att göra en till mätning.',
+  measurementCompleted3: 'Tredje och sista mätningen är klar. Tryck på Nästa för att fortsätta.',
+  measurementError: 'Något gick fel, försök igen.',
   measurementErrorMaxRetries: 'Appen kan inte mäta ditt toppflöde korrekt. Se till att enheten är ordentligt ansluten till ljuduttaget och under en ljuskälla. Om problemet kvarstår, använd Bluetooth-adaptern. Följ instruktionerna som medföljer enheten för detta.',
-  todayBest: `Today's Best Peak Flow:`,
-  results: 'Past Peak Flow Readings',
-  pef: 'Peak Flow',
-  weeks: 'Weeks'
+  todayBest: `Dagens bästa toppflödesmätning:`,
+  results: 'Tidigare toppflödesmätningar',
+  pef: 'Toppflöde',
+  weeks: 'Veckor'
 }
--- src/i18n/studies/sv/tasks/tapping.js
+++ src/i18n/studies/sv/tasks/tapping.js
@@ -1,22 +1,20 @@
 export default {
-  title: 'Finger Tapping Exercise',
-  shortDescription: 'Tap your fingers on the screen',
-  shortTitle: 'Finger Tapping',
+  title: 'Fingerfärdighetstest',
+  shortDescription: 'Tryck med fingrarna på skärmen',
+  shortTitle: 'Fingerfärdighet',
   introductionSlides: [
     {
-      title: 'Instruction',
+      title: 'Instruktioner',
       img: 'instructions/fingertapping_1.png',
-      description: 'In this task you will have to tap the screen of your phone with your index and middle fingers as fast as you can.'
+      description: 'I denna uppgift växlar du så fort du kan mellan att trycka med pekfingret och långfingret på skärmen av din telefon.'
     },
     {
-      title: 'Setup',
+      title: 'Förberedelser',
       img: 'instructions/fingertapping_1.png',
-      description: `Make sure your phone is flat on a surface before the test begins.
-      Then use the index and middle fingers of the dominant hand to alternately tap the buttons with numbers 1 and 2.
-      Keep tapping for 20 seconds and try to be as regular as possible.`
+      description: `Placera din telefon på ett plant underlag såsom ett bord före testet påbörjas. Så snabbt du kan, växla mellan att trycka på knapparna 1 och 2 med ditt pekfinger och långfinger på din dominanta hand (höger om du är högerhänt, vänster om du är vänsterhänt). Försök hålla samma takt i 20 sekunder.`
     }
   ],
-  instructions: 'Press start to begin the exercise. Once started, tap with the index and middle fingers on the two buttons at the botton of the screen as fast as you can.',
-  completed: 'Task completed',
-  summary: 'You tapped {count} times in {sec} seconds.'
+  instructions: 'Tryck på start för att påbörja övningen. När du startat, växla så snabbt du kan mellan att trycka med pekfinger och långfinger på knapparna som visas på skärmen.',
+  completed: 'Uppgiften klar.',
+  summary: 'Du tryckte {count} gånger under {sec} sekunder.'
 }
