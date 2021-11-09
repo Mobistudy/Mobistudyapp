87d5af702224296454e793226e7fbdae01bcd735
Dario Salvi <dariosalvi78@gmail.com>
2021-11-09 16:26:44 +0100

    new qcst images

--- src/i18n/studies/en/tasks/qcst.js
+++ src/i18n/studies/en/tasks/qcst.js
@@ -8,38 +8,39 @@ export default {
   introductionSlides: [
     {
       title: 'Requirements',
-      img: 'instructions/qcst_1.jpg',
+      img: 'instructions/QCST-01.svg',
       description: 'For this test, you need a sturdy step, about 40 cm (16 inches) high. This is about the height of a foot stool.'
     },
     {
       title: 'Requirements',
-      img: 'instructions/qcst_2.jpg',
+      img: 'instructions/QCST-02.svg',
       description: 'A heart rate monitor: you can use a fitness tracker, smartwatch, or you can try using an app (search for "Heart Rate" on the app store).'
     },
     {
       title: 'Requirements',
-      img: 'instructions/qcst_3.jpg',
+      img: 'instructions/QCST-03.svg',
       description: 'Alternatively, to measure your pulse manually, you can count the number of heart beats for 15 seconds and multiply the count by four.'
     },
     {
       title: 'Instruction',
-      img: 'instructions/qcst_4.jpg',
+      img: 'instructions/QCST-04.svg',
       description: 'This is a paced test with a four-step cadence: foot 1 up, foot 2 up, foot 1 down, foot 2 down.'
     },
     {
-      img: 'instructions/qcst_5.jpg',
+      img: 'instructions/QCST-05.svg',
       description: 'Step up and down following the metronome sound you will hear from the app. Make sure to turn on the sound on your device and turn up the volume.'
     },
     {
-      img: 'instructions/qcst_6.jpg',
+      img: 'instructions/QCST-06.svg',
       description: 'The test will automaticly stop after 3 minutes. If you need to complete the test earlier, press the "Complete" button.'
     },
     {
-      img: 'instructions/qcst_3.jpg',
+      img: 'instructions/QCST-07.svg',
       description: 'At the end of the test you will be asked to measure your heart rate and provide it to the app. Please measure your heart rate within 5-20 seconds after completing the test.'
     },
     {
       title: 'Important!',
+      img: 'instructions/QCST-08.svg',
       description: 'Try not to talk during the test, as this may affect your performance. Stop immediately if you have any chest pain or dizziness.'
     }
   ],
--- src/i18n/studies/sv/tasks/qcst.js
+++ src/i18n/studies/sv/tasks/qcst.js
@@ -9,38 +9,39 @@ export default {
   introductionSlides: [
     {
       title: 'Krav',
-      img: 'instructions/qcst_1.jpg',
+      img: 'instructions/QCST-01.svg',
       description: 'Inför det här testet behöver du ett stabilt trappsteg, ungefär 40cm högt. Det är ungefär höjden på en liten pall.'
     },
     {
       title: 'Krav',
-      img: 'instructions/qcst_2.jpg',
+      img: 'instructions/QCST-02.svg',
       description: 'En pulsmätare: du kan använda ett aktivitetsarmband, en fitnessklocka, eller försöka med en app (sök efter "Heart Rate" på app store).'
     },
     {
       title: 'Krav',
-      img: 'instructions/qcst_3.jpg',
+      img: 'instructions/QCST-03.svg',
       description: 'Alternativt, för att mäta din puls manuellt kan du räkna antalet hjärtslag i 15 sekunder och multiplicera det antalet med 4.'
     },
     {
       title: 'Instructions',
-      img: 'instructions/qcst_4.jpg',
+      img: 'instructions/QCST-04.svg',
       description: 'Det här är ett test med ett specifikt tempo i fyrtakt: fot 1 upp, fot 2 upp, fot 1 ner, fot 2 ner.'
     },
     {
-      img: 'instructions/qcst_5.jpg',
+      img: 'instructions/QCST-05.svg',
       description: 'Stig upp och ned i takt med metronomen du kommer höra från mobilen. Se till så att ljudet på din mobil är påslaget samt höj gärna volymen.'
     },
     {
-      img: 'instructions/qcst_6.jpg',
+      img: 'instructions/QCST-06.svg',
       description: 'Testet kommer automatiskt att avslutas efter 3 minuter. Om du behöver avsluta testet tidigare kan du trycka på "Slutför".'
     },
     {
-      img: 'instructions/qcst_3.jpg',
+      img: 'instructions/QCST-07.svg',
       description: 'I slutet av testet kommer du bli ombedd att mäta din hjärtfrekvens och skriva in informationen i appen. Var god mät din hjärtfrekvens inom 5-20 sekunder efter att du avslutat testet.'
     },
     {
       title: 'Viktigt!',
+      img: 'instructions/QCST-08.svg',
       description: 'Försök att inte prata under testets gång eftersom detta kan påverka din prestation. Sluta omedelbart om du känner smärta i bröstet eller är yr.'
     }
   ],
