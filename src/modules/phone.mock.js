c101480ac2487ca1bcbf84cb5b40a8acafa1a029
Dario Salvi <dariosalvi78@gmail.com>
2022-02-16 14:37:00 +0100

    really mocked motion and orientation

--- src/modules/phone.mock.js
+++ src/modules/phone.mock.js
@@ -126,74 +126,65 @@ export default {
     }
   },
   motion: {
-    callback: undefined,
-    motionHandler (event) {
-      let simplifiedEvent = {
-        timestamp: new Date().getTime(),
-        acc: {
-          x: event.acceleration.x,
-          y: event.acceleration.y,
-          z: event.acceleration.z
-        },
-        accG: {
-          x: event.accelerationIncludingGravity.x,
-          y: event.accelerationIncludingGravity.y,
-          z: event.accelerationIncludingGravity.z
-        },
-        rotRate: {
-          alpha: event.rotationRate.alpha,
-          beta: event.rotationRate.beta,
-          gamma: event.rotationRate.gamma
-        },
-        interval: event.interval
-      }
-      this.callback(simplifiedEvent)
-    },
+    timer: null,
     async isAvailable () {
-      if (typeof DeviceMotionEvent !== 'undefined') return Promise.resolve(true)
-      else return Promise.resolve(false)
+      return Promise.resolve(true)
     },
     async requestPermission () {
       return Promise.resolve(true)
     },
     startNotifications (options, cbk, error) {
-      this.callback = cbk
-      this.motionHandler = this.motionHandler.bind(this)
-      window.addEventListener('devicemotion', this.motionHandler, false)
+      this.timer = setInterval(() => {
+        let fakeSmaple = Math.sin((new Date().getTime() / 1000) * 2 * Math.PI)
+        cbk({
+          timestamp: new Date().getTime(),
+          acc: {
+            x: fakeSmaple,
+            y: fakeSmaple,
+            z: fakeSmaple
+          },
+          accG: {
+            x: fakeSmaple,
+            y: fakeSmaple,
+            z: fakeSmaple
+          },
+          rotRate: {
+            alpha: fakeSmaple,
+            beta: fakeSmaple,
+            gamma: fakeSmaple
+          },
+          interval: 500
+        })
+      }, 500)
     },
     async stopNotifications () {
-      window.removeEventListener('devicemotion', this.motionHandler)
-      this.callback = null
+      clearInterval(this.timer)
+      return Promise.resolve()
     }
   },
   orientation: {
-    callback: undefined,
-    orientationHandler (event) {
-      let simplifiedEvent = {
-        timestamp: new Date().getTime(),
-        abs: event.absolute,
-        alpha: event.alpha,
-        beta: event.beta,
-        gamma: event.gamma
-      }
-      if (event.webkitCompassHeading) simplifiedEvent.heading = event.webkitCompassHeading
-      this.callback(simplifiedEvent)
-    },
+    timer: undefined,
     async isAvailable () {
-      if (typeof DeviceOrientationEvent !== 'undefined') return Promise.resolve(true)
-      else return Promise.resolve(false)
+      return Promise.resolve(true)
     },
     async requestPermission () {
       return Promise.resolve(true)
     },
     startNotifications (options, cbk, error) {
-      this.callback = cbk
-      this.orientationHandler = this.orientationHandler.bind(this)
-      window.addEventListener('deviceorientation', this.orientationHandler, false)
+      this.timer = setInterval(() => {
+        let fakeSmaple = Math.sin((new Date().getTime() / 1000) * 2 * Math.PI)
+        cbk({
+          timestamp: new Date().getTime(),
+          abs: fakeSmaple,
+          alpha: fakeSmaple,
+          beta: fakeSmaple,
+          gamma: fakeSmaple
+        })
+      }, 500)
     },
     async stopNotifications () {
-      window.removeEventListener('deviceorientation', this.orientationHandler)
-      this.callback = null
+      clearInterval(this.timer)
+      return Promise.resolve()
     }
   },
 
