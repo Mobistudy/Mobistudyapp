45ef23b9367fe2fde28d062e0b4454387f2d9901
Dario Salvi <dariosalvi78@gmail.com>
2022-04-25 18:52:15 +0200

    note on sleep screen

--- src/modules/phone/screen.js
+++ src/modules/phone/screen.js
@@ -1,5 +1,6 @@
 let screen = {
   async forbidSleep () {
+    // could switch to https://developer.mozilla.org/en-US/docs/Web/API/Screen_Wake_Lock_API when supported on iOS
     return new Promise((resolve, reject) => {
       window.plugins.insomnia.keepAwake(resolve, reject)
     })
