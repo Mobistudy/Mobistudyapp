ee5fc94a3adee061eddad2edd5254e07d12496d9
Dario Salvi <dariosalvi78@gmail.com>
2024-07-18 17:33:05 +0200

    miband 3 text

---
+++ src/i18n/tasks/miband3/index.js
@@ -0,0 +1,21 @@
+import en from './en/miband3'
+import se from './se/miband3'
+import es from './es/miband3'
+
+export default {
+  en: {
+    tasks: {
+      miband3: en
+    }
+  },
+  se: {
+    tasks: {
+      miband3: se
+    }
+  },
+  es: {
+    tasks: {
+      miband3: es
+    }
+  }
+}
