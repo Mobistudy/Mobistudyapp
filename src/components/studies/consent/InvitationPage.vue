16431eaa507911d5469ad81087118e36b1ffd742
Dario Salvi <dariosalvi78@gmail.com>
2025-04-24 10:01:51 +0200

    simplified invitation page

--- src/components/studies/consent/InvitationPage.vue
+++ src/components/studies/consent/InvitationPage.vue
@@ -1,7 +1,6 @@
 <template>
   <q-page padding>
-    <div class="q-my-md mobitxt1" style="white-space: pre-line;">{{ studyDescription.consent.invitation[$i18n.locale] }}
-    </div>
+    <div class="q-my-md mobitxt1" style="white-space: pre-line;">{{ invitationText }}</div>
 
     <div class="q-my-md row justify-around">
       <q-btn class="q-mt-lg full-width mobibtn" :label="$t('common.next')" color="primary" @click="next()"></q-btn>
@@ -23,11 +22,13 @@ export default {
   },
   data () {
     return {
-      studyDescription: {}
+      invitationText: ''
     }
   },
   created () {
     const sd = session.getStudyDescription()
+    this.invitationText = sd.consent.invitation[this.$i18n.locale]
+    console.log('!!! it', this.invitationText)
     if (!sd) {
       this.$router.go(-1)
     } else {
