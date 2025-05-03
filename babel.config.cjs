4304fa4158d3f8f6b190182d92737944ff9902b4
Dario Salvi <dariosalvi78@gmail.com>
2025-05-03 11:05:20 +0200

    adding unit testing

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,14 +1,31 @@
 /* eslint-disable */
 
 module.exports = api => {
-  return {
-    presets: [
-      [
-        '@quasar/babel-preset-app',
-        api.caller(caller => caller && caller.target === 'node')
-          ? { targets: { node: 'current' } }
-          : {}
-      ]
-    ]
+  const envOptions = {};
+
+  // Options scaffolded by Quasar out of the box
+  if (api.caller((caller) => caller && caller.target === 'node')) {
+    envOptions.targets = { node: 'current' };
   }
+
+  // Only used in test environment in JS codebases
+  if (api.env() === 'test') {
+    envOptions.modules = 'commonjs';
+    envOptions.targets = { node: 'current' };
+  }
+
+  return {
+    presets: [['@quasar/babel-preset-app', envOptions]],
+  };
+
+  // return {
+  //   presets: [
+  //     [
+  //       '@quasar/babel-preset-app',
+  //       api.caller(caller => caller && caller.target === 'node')
+  //         ? { targets: { node: 'current' } }
+  //         : {}
+  //     ]
+  //   ]
+  // }
 }
